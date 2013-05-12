36fef772aba5b9e8644ce1e833c9943e0f44e7a6
Riplexus <Riplexus@gmail.com>
2013-05-12 10:44:27 +0200

    further improved the navigator

--- script/navigator.js
+++ script/navigator.js
@@ -4,7 +4,10 @@ $(function() {
     MS.navigator = {
         history: [],
 
-        initSidemenu: function() {
+        /**
+         *
+         */
+        initSidemenu: function initSidemenu() {
             // Event handler
             MS.dom.header.find('.openSidebarLeft').click(function(){
                 if(!MS.dom.body.hasClass('open-menu')) {
@@ -39,7 +42,15 @@ $(function() {
             });
         },
 
-        back: function(callback) {
+        /**
+         *
+         * @param callback
+         */
+        back: function back(callback) {
+            if (MS.navigator.history.length === 1) {
+                return;
+            }
+
             var undo = MS.navigator.history.pop();
 
             switch(undo) {
@@ -68,11 +79,19 @@ $(function() {
             }
         },
 
-        goTo: function(pagename) {
-            MS.dom.content.html($('#view_'+pagename).clone().removeAttr('id'));
+        /**
+         *
+         * @param pagename
+         */
+        goTo: function goTo(pagename) {
+            var newView = $('#view'+pagename).clone().removeAttr('id'),
+                newHeader = $('#header'+pagename);
+
+            MS.dom.content.html(newView);
+            newView.height(MS.dimens.viewport.height-MS.dimens.header.height);
+
             new iScroll(MS.dom.content.children('.view').first()[0]);
 
-            var newHeader = $('#header_'+pagename);
             if (newHeader.length > 0) {
                 MS.dom.header.html(newHeader.clone().removeAttr('id'));
                 this.initSidemenu();
@@ -81,7 +100,10 @@ $(function() {
             if(MS.dom.body.hasClass('open-menu')) {
                 MS.navigator.back();
             }
-            this.history.push(pagename);
+
+            if (MS.navigator.history[MS.navigator.history.length-1] !== pagename) {
+                MS.navigator.history.push(pagename);
+            }
         }
     };
 });
