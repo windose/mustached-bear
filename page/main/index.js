66d412028cfdcf7a43be5279af33ff61e047df18
Riplexus <Riplexus@gmail.com>
2013-05-14 17:26:52 +0200

    live debugging ftw

--- page/main/index.js
+++ page/main/index.js
@@ -1,9 +1,11 @@
 window.MS = window.MS || {};
 
 (function() {
+    alert('main script loaded');
+
     window.MS.fn = function(view, scroll) {
 
-        alert('load main script');
+        alert('main script called');
 
         view.find('li').hammer().on('tap', function() {
         //view.find('li').on('click', function() {
