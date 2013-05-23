3e6edfb00db11257a7fbec8acbe7c400d9af8344
Riplexus <Riplexus@gmail.com>
2013-05-23 11:57:18 +0200

    started with basic courses view

--- page/courses/index.js
+++ page/courses/index.js
@@ -7,13 +7,6 @@ window.MS.page = window.MS.page || {};
         init: function(header, view) {
             console.log('init courses');
 
-            var courses = view.find('ul');
-
-            view.css('width', 80*courses.length+'%');
-            view.css('padding-left', 10*courses.length+'%');
-            view.css('padding-right', 10*courses.length+'%');
-            courses.css('width', 100/courses.length*0.8+'%');
-
             /*
             view.hammer().on('swipeleft', function() {
                 var pos = 80/courses.length*0.8;
