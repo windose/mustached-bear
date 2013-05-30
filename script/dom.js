80020f59f3013e70c51e3fee158deb9bc71867e9
Riplexus <Riplexus@gmail.com>
2013-05-30 16:00:49 +0200

    added basic behaviour, without auth

--- script/dom.js
+++ script/dom.js
@@ -5,6 +5,8 @@ document.addEventListener('deviceready', function() {
     MS.dom = {
         window: $(window),
         body: $('#body'),
+        login: $('#login'),
+        loginSubmit: $('#loginSubmit'),
         sidebarLeft: $('#sidebarLeft'),
         sidebarRight: $('#sidebarRight'),
         header: $('#header'),
