d1ef4d6f26c7ec4d2af8146466b613726f81796d
Riplexus <Riplexus@gmail.com>
2013-06-09 14:47:26 +0200

    always insert dummy data

--- page/news/index.js
+++ page/news/index.js
@@ -34,55 +34,72 @@ window.MS.page = window.MS.page || {};
             });
         },
         enter: function(done, header, view) {
-            var self = this;
-
             log('enter news');
 
-            // Insert dummy data
-//            for (var i=0; i<10; i++) {
-//                MS.db.insert('nachrichten', [
-//                    'fakultaet_id',
-//                    'title',
-//                    'content',
-//                    'date',
-//                    'author',
-//                    'msg_type'
-//                ],
-//                [
-//                    7,
-//                    'Lorem ipsum dolor sit amet, consetetur',
-//                    'sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
-//                    Math.round((new Date()).getTime()/100),
-//                    'tlukacs',
-//                    '1'
-//                ], function(err) {
-//                    log('insert news', err);
-//                });
-//            }
+            var insertDummyData = function() {
+                for (var i=0; i<10; i++) {
+                    MS.db.insert('nachrichten', [
+                        'fakultaet_id',
+                        'title',
+                        'content',
+                        'date',
+                        'author',
+                        'msg_type'
+                    ],
+                    [
+                        7,
+                        'Lorem ipsum dolor sit amet, consetetur',
+                        'sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.',
+                        Math.round((new Date()).getTime()/100),
+                        'tlukacs',
+                        '1'
+                    ], function(err) {
+                        log('insert news', err);
+                    });
+                }
+            }
 
             MS.db.get('SELECT * from nachrichten', function(err, result) {
                 if (err) { return log(err) && done(); }
 
                 log('got '+result.length+' news');
 
-                var i, l;
+                if (result.length === 0) {
+                    insertDummyData();
+                    setTimeout(function() {
+                        var i, l;
+                        view.find('ul').empty();
+                        for (i=0, l=result.length; i<l; i++) {
+                            MS.page.news.insertNews(view, result[i]);
+                        }
+                        done();
+                        setTimeout(function() {
+                            log('force reflow');
+                            view.find('li').width(view.find('li').width());
+                        }, 100);
+                    }, 500);
+                } else {
 
-                view.find('ul').empty();
-                for (i=0, l=result.length; i<l; i++) {
-                    MS.page.news.insertNews(view, result[i]);
+                    var i, l;
+
+                    view.find('ul').empty();
+                    for (i=0, l=result.length; i<l; i++) {
+                        MS.page.news.insertNews(view, result[i]);
+                    }
+
+                    done();
+
+
+                    /*
+                     * Force reflow after @font-face is loaded.
+                     * text-align: justify will cut off text if we dont reflow.
+                     */
+                    setTimeout(function() {
+                        log('force reflow');
+                        view.find('li').width(view.find('li').width());
+                    }, 100);
+
                 }
-
-                done();
-
-
-                /*
-                 * Force reflow after @font-face is loaded.
-                 * text-align: justify will cut off text if we dont reflow.
-                 */
-                setTimeout(function() {
-                    log('force reflow');
-                    view.find('li').width(view.find('li').width());
-                }, 100);
             });
         },
         leave: function leave() {
